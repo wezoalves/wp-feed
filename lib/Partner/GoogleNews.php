b516e33e9e23d084ef984ab03e2d3c52870e5a91
Weslley Alves <weslley@wezo.com.br>
2024-05-28 18:06:59 -0300

    add filter icaro by tags article

--- lib/Partner/GoogleNews.php
+++ lib/Partner/GoogleNews.php
@@ -7,50 +7,22 @@ namespace Wezo\Plugin\Feed\Partner;
 use Wezo\Plugin\Feed\Core\Blog;
 use Wezo\Plugin\Feed\Core\Article;
 
-/**
- * Class GoogleNews
- * Generates RSS feed compatible with Google News.
- *
- * @package Wezo\Plugin\Feed\Partner
- */
 class GoogleNews
 {
-    /**
-     * Maximum number of articles allowed in the feed.
-     *
-     * @var int $maxArticles
-     */
     private int $maxArticles = 500;
 
-    /**
-     * Default number of articles to retrieve if not specified in the request.
-     *
-     * @var int $sizeArticles
-     */
     private int $sizeArticles = 30;
 
-    /**
-     * Default type of articles to retrieve if not specified in the request.
-     *
-     * @var array $typeArticles
-     */
     private array $typeArticles = ['post'];
 
-    /**
-     * Retrieves articles for the Google News platform.
-     *
-     * @param \WP_REST_Request $request The REST request object.
-     */
     public function callbackArticles($request)
     {
 
         $output = $request->get_param('output') ? intval($request->get_param('output')) : 'esc_html';
 
-        // Determine the limit based on the request or default size
         $limit = $request->get_param('num') ? intval($request->get_param('num')) : $this->sizeArticles;
         $limit = min($limit, $this->maxArticles);
 
-        // Set the type of articles to retrieve
         if ($request->get_param('type') && is_array($request->get_param('type'))) {
             $this->typeArticles = $request->get_param('type');
         }
@@ -58,7 +30,6 @@ class GoogleNews
             $this->typeArticles = [$request->get_param('type')];
         }
 
-        // Set the categories of posts to retrieve
         $categoriesSlug = [];
         if ($request->get_param('category') && is_array($request->get_param('category'))) {
             $categoriesSlug = $request->get_param('category');
@@ -67,7 +38,6 @@ class GoogleNews
             $categoriesSlug = [$request->get_param('category')];
         }
 
-        // Set the tags of posts to retrieve
         $tagsSlug = [];
         if ($request->get_param('subject') && is_array($request->get_param('subject'))) {
             $tagsSlug = $request->get_param('subject');
@@ -76,10 +46,8 @@ class GoogleNews
             $tagsSlug = [$request->get_param('subject')];
         }
 
-        // Get site information
         $site = (new Blog())->getInfo($request, true);
 
-        // Generate the RSS feed
         $xml = $this->getRss($site, $limit, $categoriesSlug, $tagsSlug);
 
         $response = new \WP_REST_Response();
@@ -90,19 +58,9 @@ class GoogleNews
         exit;
     }
 
-    /**
-     * Generates the RSS feed for Google News.
-     *
-     * @param object|null $site Blog information.
-     * @param int $limit Number of articles to retrieve.
-     * @param array $categoriesSlug Categories of posts to retrieve.
-     * @param array $tagsSlug Tags of posts to retrieve.
-     * @return string The generated RSS feed as a string.
-     */
     private function getRss($site = null, $limit = 0, $categoriesSlug = [], $tagsSlug = []) : string
     {
 
-        // Create the XML structure for the RSS feed
         $rss = new \SimpleXMLElement(
             '<?xml version="1.0" encoding="UTF-8" ?>
             <urlset 
@@ -111,38 +69,27 @@ class GoogleNews
             </urlset>'
         );
 
-        // Populate the RSS feed with articles
         $this->getItems($rss, $site, $limit, $categoriesSlug, $tagsSlug);
 
-        // Format and return the XML feed
         $dom = dom_import_simplexml($rss)->ownerDocument;
         $dom->formatOutput = true;
         return $dom->saveXML();
     }
 
-    /**
-     * Retrieves items to be included in the RSS feed.
-     *
-     * @param \SimpleXMLElement $rss The XML RSS element.
-     * @param object|null $site Blog information.
-     * @param int $limit Number of articles to retrieve.
-     * @param array $categoriesSlug Categories of posts to retrieve.
-     * @param array $tagsSlug Tags of posts to retrieve.
-     */
     private function getItems($rss, $site, $limit = 0, $categoriesSlug = [], $tagsSlug = [])
     {
-        // Retrieve the articles to include in the feed
+
         $articles = (new Article())->getLasts($limit, $this->typeArticles, $categoriesSlug, $tagsSlug);
-        
-        // Add each article to the XML RSS feed
+
+
         foreach ($articles as $post) {
             $post = (object) $post;
 
-            // Create the URL element
+
             $url = $rss->addChild('url');
             $url->addChild('loc', $post->link);
 
-            // Add news-related elements
+
             $news = $url->addChild('news:news', null, 'http://www.google.com/schemas/sitemap-news/0.9');
             $publication = $news->addChild('news:publication');
             $publication->addChild('news:name', $site->title);
@@ -151,7 +98,6 @@ class GoogleNews
             $news->addChild('news:title', $post->title);
             $news->addChild('news:keywords', $post->keywords);
 
-            // Add optional news-related elements
             $news->addChild('news:access', 'Free'); // Registration / Subscription / Free / Behind paywall
             $news->addChild('news:genres', 'Feature'); // PressRelease / Opinion / Blog / Feature / Review
         }
--- lib/Partner/Icaro.php
+++ lib/Partner/Icaro.php
@@ -7,41 +7,24 @@ namespace Wezo\Plugin\Feed\Partner;
 use Wezo\Plugin\Feed\Core\Blog;
 use Wezo\Plugin\Feed\Core\Article;
 
-/**
- * A classe Icaro é responsável por gerar e retornar um feed RSS personalizado de artigos.
- */
 class Icaro
 {
-    /**
-     * @var int $maxArticles Número máximo de artigos que podem ser retornados.
-     */
     private int $maxArticles = 500;
 
-    /**
-     * @var int $sizeArticles Número padrão de artigos a serem retornados.
-     */
     private int $sizeArticles = 30;
 
-    /**
-     * @var array $typeArticles Tipos de artigos a serem retornados.
-     */
     private array $typeArticles = ['post'];
 
-    /**
-     * Método que serve como callback para retornar artigos em formato RSS.
-     *
-     * @param \WP_REST_Request $request Objeto de requisição do WordPress REST API.
-     */
     public function callbackArticles($request)
     {
 
         $output = $request->get_param('output') ? intval($request->get_param('output')) : 'esc_html';
 
-        // Determina o limite baseado na requisição ou usa o tamanho padrão
+
         $limit = $request->get_param('num') ? intval($request->get_param('num')) : $this->sizeArticles;
         $limit = min($limit, $this->maxArticles);
 
-        // Define o tipo de artigos a serem recuperados
+
         if ($request->get_param('type') && is_array($request->get_param('type'))) {
             $this->typeArticles = $request->get_param('type');
         }
@@ -49,7 +32,7 @@ class Icaro
             $this->typeArticles = [$request->get_param('type')];
         }
 
-        // Define as categorias dos posts a serem recuperados
+
         $categoriesSlug = [];
         if ($request->get_param('category') && is_array($request->get_param('category'))) {
             $categoriesSlug = $request->get_param('category');
@@ -58,11 +41,18 @@ class Icaro
             $categoriesSlug = [$request->get_param('category')];
         }
 
-        // Obtém informações do site
+
+        $tagsSlug = [];
+        if ($request->get_param('subject') && is_array($request->get_param('subject'))) {
+            $tagsSlug = $request->get_param('subject');
+        }
+        if ($request->get_param('subject') && ! is_array($request->get_param('subject'))) {
+            $tagsSlug = [$request->get_param('subject')];
+        }
+
         $site = (new Blog())->getInfo($request, true);
 
-        // Gera o RSS
-        $xml = $this->getRss($site, $limit, $categoriesSlug);
+        $xml = $this->getRss($site, $limit, $categoriesSlug, $tagsSlug);
 
         $response = new \WP_REST_Response();
         $response->set_data($xml);
@@ -72,15 +62,7 @@ class Icaro
         exit;
     }
 
-    /**
-     * Gera o RSS dos artigos.
-     *
-     * @param object|null $site Informações do site.
-     * @param int $limit Limite de artigos a serem retornados.
-     * @param array $categoriesSlug Categorias dos artigos a serem retornados.
-     * @return string XML do feed RSS.
-     */
-    private function getRss($site = null, $limit = 0, $categoriesSlug = []) : string
+    private function getRss($site = null, $limit = 0, $categoriesSlug = [], $tagsSlug = []) : string
     {
         $rss = new \SimpleXMLElement('<?xml version="1.0" encoding="utf-8" ?><rss version="2.0"></rss>');
         $rss->addAttribute('xmlns:atom', 'http://www.w3.org/2005/Atom');
@@ -100,7 +82,7 @@ class Icaro
         $atomLinkElement->addAttribute('rel', 'self');
         $atomLinkElement->addAttribute('type', 'application/rss+xml');
 
-        $this->getItems($rss, $limit, $categoriesSlug);
+        $this->getItems($rss, $limit, $categoriesSlug, $tagsSlug);
 
         $dom = dom_import_simplexml($rss)->ownerDocument;
         $dom->formatOutput = true;
@@ -108,21 +90,13 @@ class Icaro
         return $dom->saveXML();
     }
 
-    /**
-     * Adiciona itens ao RSS.
-     *
-     * @param \SimpleXMLElement $rss Objeto SimpleXMLElement representando o RSS.
-     * @param object $site Informações do site.
-     * @param int $limit Limite de artigos a serem adicionados.
-     * @param array $categoriesSlug Categorias dos artigos a serem adicionados.
-     */
-    private function getItems($rss, $limit = 0, $categoriesSlug = [])
+    private function getItems($rss, $limit = 0, $categoriesSlug = [], $tagsSlug = [])
     {
         if (! $rss) {
             return;
         }
 
-        $articles = (new Article)->getLasts($limit, $this->typeArticles, $categoriesSlug);
+        $articles = (new Article)->getLasts($limit, $this->typeArticles, $categoriesSlug, $tagsSlug);
 
         foreach ($articles as $post) :
             $post = (object) $post;
